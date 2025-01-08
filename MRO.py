cf0ad32fabb9d0e8ec302947019d6abf951cd784
Vicent Caselles <vcasellesb@uoc.edu>
2025-01-08 16:08:58 +0100

    init

---
+++ MRO.py
@@ -0,0 +1,41 @@
+class A:
+    def __init__(self):
+        print('Running __init__ from class A')
+    
+    def method1(self):
+        print('Running method 1 from class A')
+    
+    def method2(self):
+        print('Running method 2 from class A')
+    
+    def method3(self):
+        print('Running method 3 from class A')
+
+class B(A):
+    def __init__(self):
+        super().__init__()
+        print('Running __init__ from class B')
+    
+    def method1(self):
+        print('Running method 1 from class B')
+    
+    def method2(self):
+        print('Running method 2 from class B')
+
+class C(B):
+    def __init__(self):
+        super().__init__()
+        print('Running __init__ from class C')
+
+class D(A):
+    def method3(self):
+        print('Running method 3 from class D')
+
+class E(C, D):
+    pass
+
+if __name__ == "__main__":
+    e = E()
+    e.method1() # Running method 1 from class B
+    e.method2() # Running method 2 from class B
+    e.method3() # Running method 3 from class D
---
+++ README.md
@@ -0,0 +1,4 @@
+# Simple example of Multiple Resolution Order
+Sources:
+* https://python-history.blogspot.com/2010/06/method-resolution-order.html
+* https://stackoverflow.com/questions/3277367/how-does-pythons-super-work-with-multiple-inheritance
